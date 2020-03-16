47715d9d52b5cd1cba142fdf8d9d004ca72f6289
Mateuskwz <mateus.takeshi@ykt.com.br>
2020-03-16 15:55:45 -0300

    Teste

---
+++ frontend/public/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDQojgqCMGEDSpwTJ4mpHHsXufEYjuektU",
+    authDomain: "tcc-2-6e00f.firebaseapp.com",
+    databaseURL: "https://tcc-2-6e00f.firebaseio.com",
+    projectId: "tcc-2-6e00f",
+    storageBucket: "tcc-2-6e00f.appspot.com",
+    messagingSenderId: "458947470097",
+    appId: "1:458947470097:web:d398f632c946a238f2a2b0",
+    measurementId: "G-BHPHV1VYC4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      Oie galerinha
+    </div>
+  );
+}
+
+export default App;
